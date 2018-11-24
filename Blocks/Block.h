601518456fd5ff58cb5cd0102a9f29b68c37452f
Anjali Notaney <anotaney@edu.uwaterloo.ca>
2018-11-24 17:04:39 -0500

    added CW rotation method to Block | removed Block friend from Grid.h

--- Blocks/Block.h
+++ Blocks/Block.h
@@ -1,16 +1,22 @@
 #ifndef __BLOCK_H__
 #define __BLOCK_H__
 
-#include "vector"
+#include <vector>   //why "" not <>
 #include <iostream>
 #include "../Grid/GridCell.h"
 #include "../Grid/Grid.h"
 
 class Block {
-    protected:
     std::vector<GridCell*> blockCells;
-    Grid* grid;
+    Grid *grid;
     char type;
+    std::vector<int> xCoords;
+    std::vector<int> yCoords;
+    std::pair<int, int> origBottomLeft;
+    std::pair<int, int> topLeft;
+    std::pair<int, int> newBottomLeft; 
+    void shiftCoordinates (std::vector<int> &, int shift);
+
     public:
     Block(Grid* g, std::vector<GridCell*> cells, char type);
     ~Block();
