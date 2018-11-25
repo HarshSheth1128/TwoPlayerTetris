5529fb6a33f29a036a503ac122867a0102c0e3ec
Anjali Notaney <anotaney@edu.uwaterloo.ca>
2018-11-24 21:44:56 -0500

    Created Interface and blank implementation files for Player abstract and concrete class

---
+++ Player/gamePlayer.cc
@@ -0,0 +1,22 @@
+//GamePlayer::GamePlayer(xWindow &w){}
+GamePlayer::~GamePlayer(){}
+
+std::vector<GridCell> GamePlayer::getRow(int rowNum){}
+
+int GamePlayer::getLevel() {}
+
+int GamePlayer::getScore() {}
+
+Block* GamePlayer::getNextBlock() {}
+
+void GamePlayer::setNextBlock(Block *) {}
+
+void GamePlayer::moveLeft() {}
+
+void GamePlayer::moveRight() {}
+
+void GamePlayer::moveDown() {}
+
+void GamePlayer::rotate(std::string direction){}
+
+void GamePlayer::drop() {}
